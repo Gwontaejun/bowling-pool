2b15d30e03c073620cf20ff21743e1491d8c5eb6
권태준 <44458173+Gwontaejun@users.noreply.github.com>
2024-04-17 16:09:34 +0900

    Chore: api 업로드를 위한 fetch 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import SearchList from '@src/components/main/searchList';
 
 const Home = async () => {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getList`, {
-    next: { revalidate: 3600 },
-  }).then((r) => r.json());
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getList`, {
+  //   next: { revalidate: 3600 },
+  // }).then((r) => r.json());
 
   return (
     <div>
-      <SearchList list={data.list} />
+      <SearchList list={[]} />
     </div>
   );
 };
