e346978d2337b0b75ef68c33f8143454cbe49e59
권태준 <hope10204@daum.net>
2025-02-18 17:34:53 +0900

    Feat: Tailwind 추가

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,12 @@
-import Link from 'next/link';
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div>
-      <Link href="/">MAIN</Link>
+      <Link className="ml-12 bg-red-700" href="/">
+        MAIN
+      </Link>
       <Link href="/search">Search</Link>
       <Link href="/teamMake">TEAMMAKE</Link>
     </div>
